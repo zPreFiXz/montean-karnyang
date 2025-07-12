cec0eacddcc44dac8ecfaf46aca483b1116fd677
Phakphoom Sukkhachat <prefix.2pm@gmail.com>
2025-07-12 12:55:25 +0700

    Update vehicle routes to use port 3000

--- server/routes/vehicle.js
+++ server/routes/vehicle.js
@@ -10,29 +10,29 @@ const {
   deleteVehicle,
 } = require("../controllers/vehicle");
 
-// @ENDPOINTS   http://localhost:5000/api/vehicle
+// @ENDPOINTS   http://localhost:3000/api/vehicle
 // Methods GET
 // ACCESS Private
 router.get("/vehicle", getAllVehicles);
 
-// @ENDPOINTS   http://localhost:5000/api/vehicle/1
+// @ENDPOINTS   http://localhost:3000/api/vehicle/1
 // Methods GET
 // ACCESS Private
 router.get("/vehicle/:id", getVehicleById);
 
-// @ENDPOINTS   http://localhost:5000/api/vehicle
+// @ENDPOINTS   http://localhost:3000/api/vehicle
 // Methods POST
 // ACCESS Private
 router.post("/vehicle", createVehicle);
 
-// @ENDPOINTS   http://localhost:5000/api/vehicle/1
+// @ENDPOINTS   http://localhost:3000/api/vehicle/1
 // Methods PUT
 // ACCESS Private
-router.post("/vehicle", updateVehicle);
+router.put("/vehicle", updateVehicle);
 
-// @ENDPOINTS   http://localhost:5000/api/vehicle/1
+// @ENDPOINTS   http://localhost:3000/api/vehicle/1
 // Methods DELETE
 // ACCESS Private
-router.post("/vehicle", deleteVehicle);
+router.delete("/vehicle", deleteVehicle);
 
 module.exports = router;
